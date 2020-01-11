bb0e009ca7d9122eb7a6e7ec47fc9be55865408a
Bryce Boe <bbzbryce@gmail.com>
2020-01-10 23:23:08 -0800

    Use integer math instead of string

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ class Calculator extends React.Component {
     }
 
     onChange = event => {
-        this.setState({[event.target.name]: event.target.value});
+        let value = parseInt(event.target.value) || "";
+        this.setState({[event.target.name]: value});
     }
 
     render() {
