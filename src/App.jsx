bb0e00cdd188260ea0ce37767c49a6c4ad4f6067
Bryce Boe <bbzbryce@gmail.com>
2020-01-10 23:55:29 -0800

    Add calculator select

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calculator />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            calculators: 1
+        }
+    }
+
+    updateCalculators = (event) => {
+        this.setState({calculators: parseInt(event.target.value)});
+    }
+
+    render() {
+        let calculators = [...Array(this.state.calculators)].map((_, i) => <Calculator />);
+        return (
+        <div className="App">
+          <header className="App-header">
+            <CalculatorSelect calculators={this.state.calculators} onChange={this.updateCalculators} />
+            {calculators}
+          </header>
+        </div>
+        );
+    }
+}
+
+class CalculatorSelect extends React.Component {
+    render() {
+        return (
+            <div>
+                1<input max="5" min="1" onChange={this.props.onChange} type="range" value={this.props.calculators} />5
+            </div>
+        );
+    }
 }
 
 class Calculator extends React.Component {
